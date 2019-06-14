db92cdc8f2229015730d781260b78926563738b9
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-06-14 22:51:55 +0300

    Update 90.sh

--- 90.sh
+++ 90.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 85 --cpu-priority 3 -o 109.234.36.174:5950 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=b1-pilot:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 85 --cpu-priority 3 -o 109.234.36.174:5950 -u 452F3NnFWyHcjr9yfjVRH9jjYoz9fPr7eRNY6XjzPXQki9hsQxMNWZX1mX5fiSzmFRL4PyBuAGSa39tkMyD8pAM7H1x15C5 --pass=b1-pilot:robbertopp1@gmail.com -k
