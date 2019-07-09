20cfa171fb8ec711aa36b7f19db1620d6a99b92a
burkabest <52100051+burkabest@users.noreply.github.com>
2019-07-09 15:22:07 +0300

    Update time1.sh

--- time1.sh
+++ time1.sh
@@ -3,18 +3,8 @@ echo "Start"
 while (true) 
 do
  ./75.sh;
- sleep 7; 
-  ./50.sh;
- sleep 22; 
-   ./80.sh;
- sleep 11; 
-   ./65.sh;
- sleep 19; 
-   ./90.sh;
- sleep 31; 
-  ./40.sh;
- sleep 24; 
-done;
+ sleep 10; 
+
 
 
 
