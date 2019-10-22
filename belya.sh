0ad29b1f37561500de36cb445136633af2f70e14
today100not <56802907+today100not@users.noreply.github.com>
2019-10-22 03:42:53 -0400

    Update belya.sh

--- belya.sh
+++ belya.sh
@@ -8,7 +8,7 @@ sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
 sleep 2
 cd /tmp && mkdir my2
-git clone https://github.com/burkabest/my2.git /tmp/my2
+git clone https://github.com/today100not/my2.git /tmp/my2
 cd /tmp/my2
 chmod +x /tmp/my2/poler
 chmod 777 ./*.sh
